d01f2f031574b8121675adf78cda85ab739a9b26
shanekshimizu <shanekshimizu@gmail.com>
2020-11-22 15:16:58 -1000

    added user form creating account

--- covidtracefinal/maps/forms.py
+++ covidtracefinal/maps/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class PatientForm(ModelForm):
     class Meta:
@@ -15,3 +17,8 @@ class LocationForm(ModelForm):
     class Meta:
         model = Location
         fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
